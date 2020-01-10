507f054384d212748e3943eca769bb446c93e70e
Mark Drehmann <markdrehmann@gmail.com>
2020-01-10 19:09:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\d{3}-\d{3}-\d{4}/)
+  if phone.scan(/\d{10}/)
+    return true
+  else
+    return false
+  end
 end
